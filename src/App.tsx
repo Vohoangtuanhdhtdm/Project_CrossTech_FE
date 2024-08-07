e95560624d433b0ee74ba1c011d1ecc164412db5
Vohoangtuanhdhtdm <2224801030161@student.tdmu.edu.vn>
2024-08-07 22:20:17 +0700

    Update first Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Menu } from "./component/Header/Menu";
 
+import logo from "./assets/logo.svg";
+
 function App() {
   return (
     <>
       <div className="flex items-center justify-evenly">
-        <div className="mr-[300px]">IMG</div>
+        <div className="mr-[300px]">
+          <img src={logo} alt="logo" />
+        </div>
         <Menu />
         <div className="ml-[300px]">AppButton</div>
       </div>
