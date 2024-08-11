7e97da7f8e916adf7973dff839621c00dc18f42b
Vohoangtuanhdhtdm <2224801030161@student.tdmu.edu.vn>
2024-08-11 16:58:23 +0700

    Add UI

--- src/component/Connect/ConnectAccount.tsx
+++ src/component/Connect/ConnectAccount.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import { IoChevronForwardOutline } from "react-icons/io5";
 
 export const ConnectAccount = () => {
+  const [click, setClick] = useState<boolean>(false);
+  const handleClick = () => {
+    setClick(!click);
+  };
   return (
     <div className="w-[1008px] h-[196px] border border-orange-700 rounded-xl ">
       <div className="flex flex-col justify-start space-y-[24px] m-[24px]">
@@ -13,9 +18,22 @@ export const ConnectAccount = () => {
             <span>Learn more</span> <IoChevronForwardOutline />
           </a>
         </div>
-        <button className="w-[235px] h-[40px] bg-[#CA5C3B] text-white rounded-3xl">
-          Connect your X account
-        </button>
+        {click ? (
+          <button
+            onClick={handleClick}
+            className="w-[235px] h-[40px] bg-[#CA5C3B] text-white rounded-3xl"
+          >
+            Connect your X account
+          </button>
+        ) : (
+          <button
+            onClick={handleClick}
+            className="bg-gray-400 w-[235px] h-[40px] text-gray-600 py-2 px-8 rounded-full font-normal 
+			text-normal "
+          >
+            @vohoangtuan
+          </button>
+        )}
       </div>
     </div>
   );
